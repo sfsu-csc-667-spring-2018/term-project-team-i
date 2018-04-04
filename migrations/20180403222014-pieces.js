d57c9c5bb394fe688e76272c3e0084b5f3ee117a
jimmyh3 <jimmyhe3452@gmail.com>
2018-04-03 21:42:15 -0700

    Migration: modified xxxxxxx-pieces.js to have return per .then statements. This is to resolve the WARNING of undefined Promises.

--- migrations/20180403222014-pieces.js
+++ migrations/20180403222014-pieces.js
@@ -3,68 +3,50 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable(
-      'pieces',
-       {
-          id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-          },
-          name: {
-            type:Sequelize.STRING,
-            unique: true,
-            allowNull: false
-          },
-          faction: {
-            type:Sequelize.STRING,
-            allowNull: false,
-            constraint: true
-          }
-      }
-    ).then(() => {
-      queryInterface.addConstraint(
-        'pieces', ['faction'],
+        'pieces',
         {
-          type: 'check',
-          name: 'faction_name_constraint',
-          where: {
-            faction: ['white', 'black']
-          }
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            name: {
+                type:Sequelize.STRING,
+                unique: true,
+                allowNull: false
+            },
+            faction: {
+                type:Sequelize.STRING,
+                allowNull: false,
+                constraint: true
+            }
         }
-      );
+    ).then(() => {
+        return queryInterface.addConstraint(
+            'pieces', ['name'],
+            {
+                type: 'check',
+                name: 'piece_name_constraint',
+                where: {
+                    name: ['pawn', 'rook', 'bishop', 'knight', 'queen', 'king'],
+                }
+            }
+        );
+    }).then(() => {
+        return queryInterface.addConstraint(
+            'pieces', ['faction'],
+            {
+                type: 'check',
+                name: 'faction_name_constraint',
+                where: {
+                    faction: ['white', 'black']
+                }
+            }
+          );
     });
-
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
   },
 
-  /*
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.addConstraint(
-      'pieces', ['faction'],
-      {
-        type: 'check',
-        name: 'faction_name_constraint',
-        where: {
-          faction: ['white', 'black']
-        }
-      }
-    );
-  },
-  */
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('pieces');
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
   }
 };
