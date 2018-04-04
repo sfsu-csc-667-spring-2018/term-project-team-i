70b5efd230625216bf30c44053e15e72af9192bf
jimmyh3 <jimmyhe3452@gmail.com>
2018-04-03 22:24:59 -0700

    Corrected migrations/xxxxx.pieces.js to previous working form.

--- migrations/20180403222014-pieces.js
+++ migrations/20180403222014-pieces.js
@@ -1,74 +1,52 @@
 'use strict';
 
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-        return queryInterface.createTable(
-            'pieces',
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+        'pieces',
+        {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            name: {
+                type:Sequelize.STRING,
+                unique: true,
+                allowNull: false
+            },
+            faction: {
+                type:Sequelize.STRING,
+                allowNull: false,
+                constraint: true
+            }
+        }
+    ).then(() => {
+        return queryInterface.addConstraint(
+            'pieces', ['name'],
             {
-                id: {
-                    type: Sequelize.INTEGER,
-                    primaryKey: true,
-                    autoIncrement: true
-                },
-                name: {
-                    type: Sequelize.STRING,
-                    unique: true,
-                    allowNull: false
-                },
-                faction: {
-                    type: Sequelize.STRING,
-                    allowNull: false,
-                    constraint: true
+                type: 'check',
+                name: 'piece_name_constraint',
+                where: {
+                    name: ['pawn', 'rook', 'bishop', 'knight', 'queen', 'king'],
                 }
             }
-        ).then(() => {
-                return queryInterface.addConstraint(
-                    'pieces', ['faction'],
-                    {
-                        id: {
-                            type: Sequelize.INTEGER,
-                            primaryKey: true,
-                            autoIncrement: true
-                        },
-                        name: {
-                            type: Sequelize.STRING,
-                            unique: true,
-                            allowNull: false
-                        },
-                        faction: {
-                            type: Sequelize.STRING,
-                            allowNull: false,
-                            constraint: true
-                        }
-                    }
-                ).then(() => {
-                    return queryInterface.addConstraint(
-                        'pieces', ['name'],
-                        {
-                            type: 'check',
-                            name: 'piece_name_constraint',
-                            where: {
-                                name: ['pawn', 'rook', 'bishop', 'knight', 'queen', 'king'],
-                            }
-                        }
-                    );
-                }).then(() => {
-                    return queryInterface.addConstraint(
-                        'pieces', ['faction'],
-                        {
-                            type: 'check',
-                            name: 'faction_name_constraint',
-                            where: {
-                                faction: ['white', 'black']
-                            }
-                        }
-                    );
-                });
-            },
+        );
+    }).then(() => {
+        return queryInterface.addConstraint(
+            'pieces', ['faction'],
+            {
+                type: 'check',
+                name: 'faction_name_constraint',
+                where: {
+                    faction: ['white', 'black']
+                }
+            }
+          );
+    });
+  },
 
-            down
-    :
-        (queryInterface, Sequelize) => {
-            return queryInterface.dropTable('pieces');
-        }
-    };
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('pieces');
+  }
+};
