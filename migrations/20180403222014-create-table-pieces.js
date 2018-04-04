9386e805dc202f7eae2d22249f3cbeefa3255f4e
jimmyh3 <jimmyhe3452@gmail.com>
2018-04-03 18:45:39 -0700

    Migration: created migration files for 'pieces' table and constraints

---
+++ migrations/20180403222014-create-table-pieces.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      'pieces',
+       {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          name: {
+            type:Sequelize.STRING,
+            unique: true,
+            allowNull: false
+          },
+          faction: {
+            type:Sequelize.STRING,
+            allowNull: false,
+            constraint: true
+          }
+      }
+    );
+
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  /*
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addConstraint(
+      'pieces', ['faction'],
+      {
+        type: 'check',
+        name: 'faction_name_constraint',
+        where: {
+          faction: ['white', 'black']
+        }
+      }
+    );
+  },
+  */
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('pieces');
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
