467acc63a0932743407d9fccb30103b5c39487c8
jimmyh3 <jimmyhe3452@gmail.com>
2018-04-19 09:19:12 -0700

    Modified db/gamesDB.js - mass refactor .createGames(userId) to formally use pg-promise's Transaction functionality in order to ensure a new game creation is a single atomic process (i.e all SQL statements in batch  must succeed or it will undo). Removed other previous helper methods for .createGame(userId) as Javascript does have private functions (was using __ to signify private; will reconsider later).

--- db/gamesDB.js
+++ db/gamesDB.js
@@ -7,98 +7,73 @@ class GamesDB {
     }
 
     createGame(userId) {
-        this.__createGame()
-            .then(createGameResult => {
-                const gameId = createGameResult['id'];
-                this.__createGameUser(gameId, userId)
-                    .then(() => {
-                        this.__createGamePieces(gameId, userId);
-                    })
-                    .catch (error => {
-                        console.log(error);
-                    });
-            })
-            .catch (error => {
-                console.log(error);
-            });
-    }
-    
-    __createGame() {
         const sqlCreateGame =  `INSERT INTO games 
                                 (active, turn) 
                                 VALUES 
                                 ('active', 'white') 
                                 RETURNING id;`;
-        
-        return db.one(sqlCreateGame);
-    }
 
-    __createGameUser(gameId, userId) {
         const sqlCreateGameUser = ` INSERT INTO game_users
                                     (gameid, userid)
                                     VALUES
                                     ($1, $2);`;
 
-        return db.any(sqlCreateGameUser, [gameId, userId]);
-    }
-
-    __createGamePieces(gameId, userId) {
-
-        const specialPieces = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
-        const spawnDetails = [  {faction: "white", pawnSpawnLine: '2', specialSpawnLine: '1'},
-                                {faction: "black", pawnSpawnLine: '7', specialSpawnLine: '8'}  ];
-
-        for (let spawnDetailIdx = 0; spawnDetailIdx < spawnDetails.length; spawnDetailIdx++) {
-            for (let offset = 0; offset < 8; offset++) {
-            
-                const positionAlpha = String.fromCharCode(97 + offset);
-    
-                // INSERT pawn piece records for white
-                this.__createChessPiece('pawn', spawnDetails[spawnDetailIdx].faction)
-                    .then(createChessPieceResult => {
-                        const pieceId = createChessPieceResult['id'];
-                        this.__createGamePiece(gameId, userId, pieceId, positionAlpha, spawnDetails[spawnDetailIdx].pawnSpawnLine, true)
-                            .catch(error => {
-                                console.log('SQL - ERROR for '+this.__createGamePiece.name, error);
-                            });
-                    })
-                    .catch (error => {
-                        console.log('SQL - ERROR for '+this.__createGamePieces.name, error);
-                    });
-                    
-                this.__createChessPiece(specialPieces[offset], spawnDetails[spawnDetailIdx].faction)
-                    .then(createChessPieceResult => {
-                        const pieceId = createChessPieceResult['id'];
-                        this.__createGamePiece(gameId, userId, pieceId, positionAlpha, spawnDetails[spawnDetailIdx].specialSpawnLine, true)
-                            .catch(error => {
-                                console.log('SQL - ERROR for '+this.__createGamePiece.name, error);
-                            });
-                    })
-                    .catch (error => {
-                        console.log('SQL - ERROR for '+this.__createGamePieces.name, error);
-                    });
-            }
-        }
-    }
-
-    __createChessPiece(name, faction) {
         const sqlCreateChessPiece =   ` INSERT INTO pieces
                                         (name, faction)
                                         VALUES
                                         ($1, $2)
                                         RETURNING id;`;
 
-        return db.one(sqlCreateChessPiece, [name, faction]);
-    }
-
-    __createGamePiece(gameId, userId, pieceId, coordinate_x, coordinate_y, alive) {
         const sqlCreateGamePiece = `INSERT INTO game_pieces
                                     (gameid, userid, pieceid, coordinate_x, coordinate_y, alive)
                                     VALUES
                                     ($1, $2, $3, $4, $5, $6);`;
 
-        return db.any(sqlCreateGamePiece, [gameId, userId, pieceId, coordinate_x, coordinate_y, alive]);
+        db.tx(t1 => {
+            return t1.one(sqlCreateGame)
+                        .then(newGameRecord => {
+                            const queries = [];
+                            const gameId  = newGameRecord['id'];
+
+                            const specialPieces = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
+                            const armyDetails   = [  {faction: "white", pawnRowNum: '2', specialRowNum: '1'},
+                                                     {faction: "black", pawnRowNum: '7', specialRowNum: '8'}  ];
+                            
+                            queries.push(t1.any(sqlCreateGameUser, [gameId, userId]));
+                            
+                            for (let armyDetailsIdx = 0; armyDetailsIdx < armyDetails.length; armyDetailsIdx++) {
+                                for (let offset = 0; offset < 8; offset++) {
+                                    const alphaRow      = String.fromCharCode(97 + offset);
+                                    const faction       = armyDetails[armyDetailsIdx].faction;
+                                    const pawnRowNum    = armyDetails[armyDetailsIdx].pawnRowNum;
+                                    const specialRowNum = armyDetails[armyDetailsIdx].specialRowNum;
+
+                                    queries.push(
+                                                t1.one(sqlCreateChessPiece, ['pawn', faction])
+                                                    .then(newChessPiece => {
+                                                        const pieceId = newChessPiece['id'];
+
+                                                        return t1.any(sqlCreateGamePiece, [gameId, userId, pieceId, alphaRow, pawnRowNum, true])
+                                                    })
+                                                ,
+                                                t1.one(sqlCreateChessPiece, [specialPieces[offset], faction])
+                                                    .then(newChessPiece => {
+                                                        const pieceId = newChessPiece['id'];
+
+                                                        return t1.any(sqlCreateGamePiece, [gameId, userId, pieceId, alphaRow, specialRowNum, true])
+                                                    })
+                                                );
+                                }
+                            }
+
+                            return t1.batch(queries);
+                        });
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
+
 }
 
 module.exports = GamesDB;
