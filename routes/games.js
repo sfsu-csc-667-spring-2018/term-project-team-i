b82197723d0faf8a9857597dbc3b99dfc5d90004
jimmyh3 <jimmyhe3452@gmail.com>
2018-04-18 17:16:14 -0700

    Modified routes/games.js by removing code that has been refactored into db/gamesDB.js. This has been tested and is working as intended 4/18 5:16pm

--- routes/games.js
+++ routes/games.js
@@ -1,3 +1,4 @@
+const GamesDB = require('../db/gamesDB.js')
 const express = require("express");
 const router = express.Router();
 const db = require('../db');
@@ -5,185 +6,14 @@ const db = require('../db');
 
 // Create new game room. req.body = {playerId: int}
 router.post('/', (req, res, next) =>{
-
-    // TODO: check if Player has logged in first, otherwise REDIRECT to login screen.
-    
+    const gamesDB = new GamesDB();
     const playerId = req.body['playerId'];
 
-    const sqlCreateGame =  `INSERT INTO games 
-                            (active, turn) 
-                            VALUES 
-                            ('active', 'white') 
-                            RETURNING id;`;
-
-    const sqlCreateGameUser = ` INSERT INTO game_users
-                                (gameid, userid)
-                                VALUES
-                                ($1, $2);`;
-
-    const sqlCreateChessPiece =   ` INSERT INTO pieces
-                                    (name, faction)
-                                    VALUES
-                                    ($1, $2)
-                                    RETURNING id;`;
+    gamesDB.createGame(playerId);
     
-    const sqlCreateGamePiece = `INSERT INTO game_pieces
-                                (gameid, userid, pieceid, coordinate_x, coordinate_y, alive)
-                                VALUES
-                                ($1, $2, $3, $4, $5, $6);`;
-
-
-    db.one(sqlCreateGame)
-        .then(sqlCreateGameResult => {
-            const newGameId = sqlCreateGameResult['id'];
-            db.any(sqlCreateGameUser, [newGameId, playerId])
-                .then(() => {
-                    
-                    const faction = {   white: 'white',
-                                        black: 'black'  };
-
-                    const piecesNames = {   pawn:'pawn', 
-                                            king:'king', 
-                                            queen: 'queen', 
-                                            bishop: 'bishop', 
-                                            knight: 'knight', 
-                                            rook: 'rook'    };
-                    
-                    // Insert pawn pieces
-                    for (let positionOffset = 0; positionOffset < 8; positionOffset++) {
-                        const positionAlpha = String.fromCharCode(97 + positionOffset);
-
-                        db.one(sqlCreateChessPiece, [piecesNames.pawn, faction.white])
-                            .then(sqlCreateChessPieceResult => {
-                                const newPieceId = sqlCreateChessPieceResult['id'];
-
-                                db.any(sqlCreateGamePiece, [newGameId, playerId, newPieceId, positionAlpha, '2', true])
-                                    .catch(error => {
-                                        console.log('Create Game Piece '+piecesNames.pawn+' ERROR: ', error);
-                                    });
-                            })
-                            .catch(error => {
-                                console.log('Create Game Piece Pawn '+piecesNames.pawn+' ERROR: ', error);
-                            });
-                    }
-                    
-                    // ------------------INSERT ROOK ---------------------------------
-                    db.one(sqlCreateChessPiece, [piecesNames.rook, faction.white])
-                        .then(sqlCreateChessPieceResult => {
-                            const newPieceId = sqlCreateChessPieceResult['id'];
-
-                            db.any(sqlCreateGamePiece, [newGameId, playerId, newPieceId, 'a', '1', true])
-                                    .catch(error => {
-                                        console.log('Create Game Piece '+piecesNames.rook+' ERROR: ', error);
-                                    });
-                        })
-                        .catch(error => {
-                            console.log('Create Game Piece '+piecesNames.rook+' Left ERROR: ', error);
-                        });
-                    
-                    db.one(sqlCreateChessPiece, [piecesNames.rook, faction.white])
-                        .then(sqlCreateChessPieceResult => {
-                            const newPieceId = sqlCreateChessPieceResult['id'];
-
-                            db.any(sqlCreateGamePiece, [newGameId, playerId, newPieceId, 'h', '1', true])
-                                    .catch(error => {
-                                        console.log('Create Game Piece '+piecesNames.rook+' Right ERROR: ', error);
-                                    });
-                        })
-                        .catch(error => {
-                            console.log('Create Game Piece '+piecesNames.rook+' Right ERROR: ', error);
-                        });
-                    // --------------------------------------------------------------
-                    // --------------INSERT KNIGHT ----------------------------------
-                    db.one(sqlCreateChessPiece, [piecesNames.knight, faction.white])
-                        .then(sqlCreateChessPieceResult => {
-                            const newPieceId = sqlCreateChessPieceResult['id'];
-
-                            db.any(sqlCreateGamePiece, [newGameId, playerId, newPieceId, 'b', '1', true])
-                                    .catch(error => {
-                                        console.log('Create Game Piece '+piecesNames.knight+' ERROR: ', error);
-                                    });
-                        })
-                        .catch(error => {
-                            console.log('Create Game Piece ' + piecesNames.knight + ' ERROR: ', error);
-                        });
-
-                    db.one(sqlCreateChessPiece, [piecesNames.knight, faction.white])
-                        .then(sqlCreateChessPieceResult => {
-                            const newPieceId = sqlCreateChessPieceResult['id'];
-
-                            db.any(sqlCreateGamePiece, [newGameId, playerId, newPieceId, 'g', '1', true])
-                                    .catch(error => {
-                                        console.log('Create Game Piece '+piecesNames.knight+' ERROR: ', error);
-                                    });
-                        })
-                        .catch(error => {
-                            console.log('Create Game Piece ' + piecesNames.knight + ' ERROR: ', error);
-                        });
-                    // --------------------------------------------------------------
-                    // --------------INSERT BISHOP ----------------------------------
-                    db.one(sqlCreateChessPiece, [piecesNames.bishop, faction.white])
-                        .then(sqlCreateChessPieceResult => {
-                            const newPieceId = sqlCreateChessPieceResult['id'];
-
-                            db.any(sqlCreateGamePiece, [newGameId, playerId, newPieceId, 'c', '1', true])
-                                    .catch(error => {
-                                        console.log('Create Game Piece '+piecesNames.bishop+' ERROR: ', error);
-                                    });
-                        })
-                        .catch(error => {
-                            console.log('Create Game Piece '+piecesNames.bishop+' ERROR: ', error);
-                        });
-                    
-                    db.one(sqlCreateChessPiece, [piecesNames.bishop, faction.white])
-                        .then(sqlCreateChessPieceResult => {
-                            const newPieceId = sqlCreateChessPieceResult['id'];
-
-                            db.any(sqlCreateGamePiece, [newGameId, playerId, newPieceId, 'f', '1', true])
-                                .catch(error => {
-                                    console.log('Create Game Piece '+piecesNames.bishop+' ERROR: ', error);
-                                });
-                        }).catch(error => {
-                            console.log('Create Game Piece '+piecesNames.bishop+' ERROR: ', error);
-                        });
-                    // --------------------------------------------------------------
-                    // --------------INSERT QUEEN ----------------------------------
-                    db.one(sqlCreateChessPiece, [piecesNames.queen, faction.white])
-                        .then(sqlCreateChessPieceResult => {
-                            const newPieceId = sqlCreateChessPieceResult['id'];
-
-                            db.any(sqlCreateGamePiece, [newGameId, playerId, newPieceId, 'd', '1', true])
-                                .catch(error => {
-                                    console.log('Create Game Piece '+piecesNames.queen+' ERROR: ', error);
-                                });
-                        }).catch(error => {
-                            console.log('Create Game Piece '+piecesNames.queen+' ERROR: ', error);
-                        });
-                    // ----------------Insert King piece-----------------------------
-                    db.one(sqlCreateChessPiece, [piecesNames.king, faction.white])
-                        .then(sqlCreateChessPieceResult => {
-                            const newPieceId = sqlCreateChessPieceResult['id'];
-
-                            db.any(sqlCreateGamePiece, [newGameId, playerId, newPieceId, 'e', '1', true])
-                                .catch(error => {
-                                    console.log('Create Game Piece '+piecesNames.king+' ERROR: ', error);
-                                });
-                        }).catch(error => {
-                            console.log('Create Game Piece '+piecesNames.king+' ERROR: ', error);
-                        });
-                    
-                })
-                .catch(error => {
-                    console.log('Create Game Users ERROR:', error);
-                });
-            
-            //TODO: REDIRECT Player to /:gameId where id is the newGameId. This is effectively a Player join a game now.
-        })
-        .catch(error => {
-            console.log('Create Game ERROR:', error);
-        });
-
-    //TODO: replace this with official response later.
+    // TODO: check if Player has logged in first, otherwise REDIRECT to login screen.
+    // TODO: REDIRECT Player to /:gameId where id is the newGameId. This is effectively a Player join a game now.
+    // TODO: replace this with official response later.
     res.send("Go check postgres database.");
 });
 
