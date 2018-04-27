f6c88b80b67a8451be27f2eb319f9f979f3194c3
jimmyh3 <jimmyhe3452@gmail.com>
2018-04-26 17:36:09 -0700

    Modified db/gamesDB.js to be used in accordance to changes on 4/24.

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use((req, res, next) =>{
 app.use('/', index);
 app.use('/users', users);
 app.use('/tests', tests);
-app.use('/games/', games);
+app.use('/games', games);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) =>{
