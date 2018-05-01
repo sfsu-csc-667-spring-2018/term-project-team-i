23caf75a6255e55dd5f51962beed7759c9289203
jimmyh3 <jimmyhe3452@gmail.com>
2018-04-30 21:38:53 -0700

    Modified games.handlebars to make use of handlebars block functions to generate the chessboard without hardcoding the HTML skeleton. Modified routes/games.js to pass necessary handlebar block helper functions to enable games.handlebars to generate the chessboard. TODO: remove routes/games.js route.get(/, ()) {}; function as it's only for testing.

--- routes/games.js
+++ routes/games.js
@@ -4,26 +4,73 @@ const router = express.Router();
 const db = require('../db');
 const gamesDB = new GamesDB();
 
+// TODO: this is a test route
 router.get('/', (req, res, next) =>{
-   res.render('games');
+
+    const handlebarsData = {};
+
+    handlebarsData.helpers = {
+        // Add all helper functions here.
+        chessboard_setupBlackOrWhiteClass: (blackClass, whiteClass, cellAlpha, cellNumber) => {
+            const alphaNumber = cellAlpha.charCodeAt(0);
+            const numberNumber = cellNumber;
+            const isCellAlphaEven = (alphaNumber % 2 === 0);
+            const isCellNumberEven = (cellNumber % 2 === 0);
+
+            return ((isCellAlphaEven && isCellNumberEven) || (!isCellAlphaEven && !isCellNumberEven)) ? whiteClass : blackClass;
+        },
+
+        chessboard_setupBorderAscii: (base, increments, count, block) => {
+            let accumulate = '';
+            for (let idx = 0; idx <= count; idx++) {
+                accumulate += block.fn(String.fromCharCode(base));
+                base += increments;
+            }
+            return accumulate;
+        }
+    }
+
+    res.render('games', handlebarsData);
 });
+
 // Create new game room. req.body = {playerId: int}
 router.post('/', (req, res, next) =>{
-    const playerId = req.body['playerId'];
+    const playerId = req.user.id;
+    const handlebarsData = {};
 
-    gamesDB.createGame(playerId);
+    handlebarsData.helpers = {
+        // Add all helper functions here.
+        chessboard_setupBlackOrWhiteClass: (blackClass, whiteClass, cellAlpha, cellNumber) => {
+            const alphaNumber = cellAlpha.charCodeAt(0);
+            const numberNumber = cellNumber;
+            const isCellAlphaEven = (alphaNumber % 2 === 0);
+            const isCellNumberEven = (cellNumber % 2 === 0);
+
+            return ((isCellAlphaEven && isCellNumberEven) || (!isCellAlphaEven && !isCellNumberEven)) ? whiteClass : blackClass;
+        },
+
+        chessboard_setupBorderAscii: (base, increments, count, block) => {
+            let accumulate = '';
+            for (let idx = 0; idx <= count; idx++) {
+                accumulate += block.fn(String.fromCharCode(base));
+                base += increments;
+            }
+            return accumulate;
+        }
+    }
+
+    gamesDB.createGame(playerId, (gameId) => {
+        res.render('games/'+gameId, handlebarsData);
+    });
     
     // TODO: check if Player has logged in first, otherwise REDIRECT to login screen.
     // TODO: REDIRECT Player to /:gameId where id is the newGameId. This is effectively a Player join a game now.
     // TODO: replace this with official response later.
-    res.send("Go check postgres database.");
 });
-
-
 
 // Join a game room. req.params = {"gameId": int} req.body = {playerId: int}
 router.post('/:gameId', (req, res, next) => {
-
+    console.log("Reached games. The given id is : " + req.params.gameId);
     /*
     Programming flow:
         -Check Player is logged in.
