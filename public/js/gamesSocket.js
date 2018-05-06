e6b63ea31a6d5b8fac7639da5e4412d0d9fb401b
Razmik Hakobyan <razhakobyan@gmail.com>
2018-05-05 20:38:05 -0700

    updated auth redirect

--- public/js/gamesSocket.js
+++ public/js/gamesSocket.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    const gameSocket = io('/games/' + '');
+    const url = window.location.pathname.split('/games/')[1];
+    const gameSocket = io('/games/' + url);
     // const $gameMessageForm = $('#gameMessageForm');
     const $gameMessage = $('#gameMessage');
     const $gameChat = $('#gameChat');
@@ -7,7 +8,7 @@ $(document).ready(function(){
 
     //game message
     gameSocket.on('new game message', data =>{
-        $gameChat.prepend('<div class="gameChat>' + data.gameUser + ':' + data.gameMsg + '</div>' );
+        $gameChat.prepend('<div class="chat" id="gameChat">' + data.gameUser + ': ' + data.gameMsg + '</div>' );
         $gameMessage.val('');
     })
 });
