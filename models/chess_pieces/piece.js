4a447587d42d2c932db1bc1123e1666dc538551b
jimmyh3 <jimmyhe3452@gmail.com>
2018-05-12 14:08:03 -0700

    Modified chess_pieces/piece.moveToPosition to isValidMovement(); isValidMovement simply indicates true or false as opposed to the original moveToPosition() whose intent was to validate and finalize the position change. Unfortunately that was problematic as the database object does not reside on each Piece object.

--- models/chess_pieces/piece.js
+++ models/chess_pieces/piece.js
@@ -16,8 +16,16 @@ class Piece {
         this.alive = gamePieceRecord.alive;
     }
     
-    moveToPosition(newCoordinateX, newCoordinateY, otherConditions) {
-        throw new Error(this.moveToPosition.name + " is abstract and must be implemented.");
+    /**
+     * Determines if the given destination coordinates are allowed to be moved
+     * into by this piece's movement rules.
+     * @param {String} newCoordinateX The destination x coordinates to move to.
+     * @param {String} newCoordinateY The destination y coordinates to move to.
+     * @param {Array} allGamePieces The other active game pieces on the board to check for collisions.
+     * @param {Object} otherConditions Optional parameters for potential fringe cases.
+     */
+    isValidMovement(newCoordinateX, newCoordinateY, allGamePieces = [], otherConditions) {
+        throw new Error(this.isValidMovement.name + " is abstract and must be implemented.");
     }
 
 }
