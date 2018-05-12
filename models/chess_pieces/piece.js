be413453862b915bb0b35fff739bf79de925d7ba
jimmyh3 <jimmyhe3452@gmail.com>
2018-05-12 10:27:24 -0700

    Modified piece.js to use a single gamePieceRecord only as constructor as opposed to a series fields. This is simply for flexibility later.

--- models/chess_pieces/piece.js
+++ models/chess_pieces/piece.js
@@ -1,16 +1,19 @@
 class Piece {
 
-    constructor (name, faction, coordinate_x, coordinate_y, alive) {
+    constructor (gamePieceRecord) {
         
         if (this.constructor === Piece) {
             throw new TypeError('Abstract class "Piece" cannot be instantiated.');
         }
         
-        this.name = name;
-        this.faction = faction;
-        this.coordinate_x = coordinate_x;
-        this.coordinate_y = coordinate_y;
-        this.alive = alive;
+        this.gameId = gamePieceRecord.gameid;
+        this.userId = gamePieceRecord.userid;
+        this.pieceId = gamePieceRecord.pieceid;
+        this.name = gamePieceRecord.name;
+        this.faction = gamePieceRecord.faction;
+        this.coordinate_x = gamePieceRecord.coordinate_x;
+        this.coordinate_y = gamePieceRecord.coordinate_y;
+        this.alive = gamePieceRecord.alive;
     }
     
     moveToPosition(newCoordinateX, newCoordinateY) {
