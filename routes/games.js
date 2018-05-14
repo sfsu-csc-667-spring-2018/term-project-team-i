807b92a14d3baefeea44b6b543e39071dc753a20
jimmyh3 <jimmyhe3452@gmail.com>
2018-05-14 04:05:53 -0700

    Mod route for creating and joining a game to make use of GameManager.

--- routes/games.js
+++ routes/games.js
@@ -1,93 +1,72 @@
 const express = require("express");
 const handlebars = require('express-handlebars');
 const GamesDB = require('../db/gamesDB.js');
+const GameManager = require('../models/gameManager.js');
 const GamesHbsHelpers = require('../routes/gamesControllers/gamesRenderHelpers.js');
 const GameMoveValidator = require('../routes/gamesControllers/gameMoveValidator.js');
+const Game = require('../models/game.js');
 const router = express.Router();
 const gamesDB = new GamesDB();
+const gameManager = new GameManager();
 const gameMoveValidator = new GameMoveValidator();
 const auths = require('../auth/authenticate');
 const gameAuth = require('../auth/gameAuth');
-const activeGames = {};
 
+const activeGames = new Map();
 
 // Create new game room. req.body = {playerId: int}
 router.post('/', auths, (req, res, next) => {
-    // TODO: check if Player has an ID/exists via req.user.id
-    // TODO: check if Player has logged in first, otherwise REDIRECT to login screen.
-    // TODO: REDIRECT Player to /:gameId where id is the newGameId. This is effectively a Player join a game now.
-    // TODO: replace this with official response later.
-    
     const playerId = req.user.id;
-    gamesDB.createNewGame(playerId, 'white', (gameId) => {
-        //res.end('/games/'+gameId);
-        res.redirect('/games/'+gameId);  
+
+    gameManager.createGameInstance(playerId, 'white', (gameId) => {
+        res.redirect('/games/'+gameId);
     });
 });
 
 // Join a game room. req.params = {"gameId": int} req.body = {playerId: int}
 router.get('/:gameId', auths, (req, res, next) => {
-    const userid = req.user.id;
+    const playerId = req.user.id;
     const gameId = req.params.gameId;
     const renderData = {};
-    let gameConnections =[];
 
-    gameAuth(userid, gameId,
-    () => {
-        //success handler
-        res.app.get('io').of('/games/' + gameId).on('connection',socket =>{
-            gameConnections.push(socket);
-            console.log('Connected to game: %s sockets connected', gameConnections.length);
+    gameManager.getGameInstance(gameId, (returnedGameInstance) => {
+        /** @type {Game} */
+        const gameInstance = returnedGameInstance;
+        const gameHostId = gameInstance.hostId;
+        const gameOpponentId = gameInstance.opponentId;
 
-            socket.on('disconnect', () => {
-                gameConnections.splice(gameConnections.indexOf(socket), 1);
-                console.log('Game Disconnected: %s sockets connected', gameConnections.length);
+        const funcSuccess = () => {
+
+            // Game room Socket protocols
+            res.app.get('io').of('/games/' + gameId).on('connection',socket =>{
+                socket.on('disconnect', () => {
+                    console.log("Player left + " + playerId);
+                });
             });
-        });
 
-        renderData.helpers = GamesHbsHelpers.getHandlebarHelpers();
-
-        gamesDB.getGamePiecesAlive(gameId, (gamePieceRecords) => {
-            renderData.gamePieces = GamesHbsHelpers.toCellGamePieceObject(gamePieceRecords);
+            renderData.helpers = GamesHbsHelpers.getHandlebarHelpers();
+            renderData.gamePieces = GamesHbsHelpers.toCellGamePieceObject(gameInstance.gamePiecesRecords);
             renderData.gameId = gameId;
+
             res.render('games',renderData);
-        })
-    },
-    () => {
-        //failure handler
-        req.flash('error_msg', 'You are not allowed in this game');
-        res.redirect('/');
+        }
+
+        const funcFailure = () => {
+            req.flash('error_msg', 'You are not allowed in this game');
+            res.redirect('/');
+        }
+
+
+        if (gameHostId == playerId || gameOpponentId == playerId) {
+            funcSuccess();
+        } else if (gameHostId != playerId && gameOpponentId == null) {
+            gameInstance.setOpponentID(playerId);
+            funcSuccess();
+        } else {
+            funcFailure();
+        }
     });
 
-    /*
-    1. Use the given gameId to pass off the information to an appropriate handler.
-    2. Get from database all the pieces that belongs to the game.
-    2. Process each record to place each piece in accordance to its location on the screen.
-    3. Send the rendered information to the user.
-    4.
-    */
-
-    /*
-    Programming flow:
-        -Check Player is logged in.
-            If Player is logged in:
-                -SELECT * FROM game_users WHERE gameid='gameId' // AND (userid='playerId' OR opponentid='playerId');
-
-                    If successful and the record is found (promise) then
-                        If ['userid'] == 'playerId'
-                            -Player is white (and assumed as original host).
-                            -SELECT * FROM game_pieces WHERE gameid='gameId'     //Initialize prior Game state.
-                                If no record found then error; Redirect to lobby.
-
-                        Else If ['opponentid'] == NULL || ['opponentid'] == 'playerId'  //'opponent' == NULL also implies the host Player is waiting since the record is destroyed if the host did not wait for an opponent.
-                            -Player is black and id is (re)inserted into database as opponent (regardless if it has been set before to cover both cases).
-                            -SELECT * FROM game_pieces WHERE gameid='gameId'     //Initialize prior Game state.
-                                If no record found then error; Redirect to lobby.
-                    Else
-                        Redirect to lobby as game creation must happen via Create Game Button.
-            Else
-                -Redirect to Sign In page.
-    */
 });
 
 // Sends a message in the Game Room.
