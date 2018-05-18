e2ee868a9db04d5445c3f295c3e32938ef4bb58e
jimmyh3 <jimmyhe3452@gmail.com>
2018-05-17 21:02:45 -0700

    Modified pawn.js - pawn isValidateMovement() is implemented; can move two spaces as initial move or just one as a regular move (assuming no piece is blocking), capture diagonally left/right.

--- models/chess_pieces/pawn.js
+++ models/chess_pieces/pawn.js
@@ -5,18 +5,17 @@ class Pawn extends Piece {
     constructor (gamePieceRecord) {
         super(gamePieceRecord);
 
-        this.faction_white = 'white';
-        this.faction_black = 'black';
-        this.isInitialMove = this.__isInitialMove();
+        this.name_white = 'white';
+        this.name_black = 'black';
     }
 
-    __isInitialMove() {
+    get isInitialMove() {
         let isInitialMove = false;
 
-        if (this.faction === 'white') {
-            isInitialMove = (this.coordinate_y === '2');
-        } else if (this.faction === 'black') {
-            isInitialMove = (this.coordinate_y === '7');
+        if (this.faction === this.name_white) {
+            isInitialMove = (this.raw_coordinate_y === '2');
+        } else if (this.faction === this.name_black) {
+            isInitialMove = (this.raw_coordinate_y === '7');
         }
 
         return isInitialMove;
@@ -25,14 +24,14 @@ class Pawn extends Piece {
     /**
      * Determines if the given destination coordinates are allowed to be moved
      * into by this piece's movement rules.
-     * @param {String} destination_x The destination x coordinates to move to.
-     * @param {String} destination_y The destination y coordinates to move to.
+     * @param {Number} idx_destination_x The destination x coordinates to move to in NUMBER form (0-7).
+     * @param {Number} idx_destination_y The destination y coordinates to move to in NUMBER form (0-7).
      * @param {Number} directionContraint The direction of which this piece can only move.
      * @param {Array} chessboard The other active game pieces on the board to check for collisions.
      * @param {Object} otherConditions Optional parameters for potential fringe cases.
      * @returns {boolean} A boolean result indicate if the movement was valid.
      */
-    __isValidMovement(destination_x, destination_y, directionContraint, chessboard = [], otherConditions) {
+    __isValidMovement(idx_destination_x, idx_destination_y, directionContraint, chessboard = [], otherConditions) {
         
         /**
          * This helper function is used to determine if the given position has an enemy
@@ -41,8 +40,8 @@ class Pawn extends Piece {
          * such as moving foward, would deny movement if ANY piece existed at the cell.
          */
         const funcNoPiecePresenceCheck = (colInc, rowInc, chessboard, allowedIfEnemy) => {
-            const at_x = this.coordinate_x_number + colInc;
-            const at_y = this.coordinate_y_number + rowInc;
+            const at_x = this.coordinateXConverted + colInc;
+            const at_y = this.coordinateYConverted + rowInc;
             /** @type {Piece} */
             const possiblePiece = chessboard[at_x][at_y];
 
@@ -53,13 +52,13 @@ class Pawn extends Piece {
             }
         }
 
-        const isDestinationDiagRight = ( (destination_x == (this.coordinate_x_number + 1)) && (destination_y == (this.coordinate_y_number + 1*(directionContraint))) );
+        const isDestinationDiagRight = ( (idx_destination_x == (this.coordinateXConverted + 1)) && (idx_destination_y == (this.coordinateYConverted + 1*(directionContraint))) );
         const theresEnemyDiagRight = funcNoPiecePresenceCheck(1, 1*directionContraint, chessboard, true);
-        const isDestinationDiagLeft = ( (destination_x == (this.coordinate_x_number - 1)) && (destination_y == (this.coordinate_y_number + 1*(directionContraint))) );
+        const isDestinationDiagLeft = ( (idx_destination_x == (this.coordinateXConverted - 1)) && (idx_destination_y == (this.coordinateYConverted + 1*(directionContraint))) );
         const theresEnemyDiagLeft = funcNoPiecePresenceCheck(-1, 1*directionContraint, chessboard, true);
-        const isDestinationForwardOne = ( (destination_x == (this.coordinate_x_number - 0)) && (destination_y == (this.coordinate_y_number + 1*(directionContraint))) );
+        const isDestinationForwardOne = ( (idx_destination_x == (this.coordinateXConverted - 0)) && (idx_destination_y == (this.coordinateYConverted + 1*(directionContraint))) );
         const theresNoPieceAtForwardOne = funcNoPiecePresenceCheck(0, 1*directionContraint, chessboard, false);
-        const isDestinationForwardTwo = ( (destination_x == (this.coordinate_x_number - 0)) && (destination_y == (this.coordinate_y_number + 2*(directionContraint))) );
+        const isDestinationForwardTwo = ( (idx_destination_x == (this.coordinateXConverted - 0)) && (idx_destination_y == (this.coordinateYConverted + 2*(directionContraint))) );
         const theresNoPieceAtForwardTwo = (this.isInitialMove) ? funcNoPiecePresenceCheck(0, 2*directionContraint, chessboard, false) : false;
 
         // Now this is refined autism.
@@ -73,23 +72,23 @@ class Pawn extends Piece {
      * Determines if the given destinations are valid positions the piece can move to.
      * The Pieces will also determine if there are blocking pieces in its path that prevents
      * it from moving the destination.
-     * @param {Number} destination_x The x coordinate destination in NUMBER form (0 to 7).
-     * @param {Number} destination_y The y coordinate destination in NUMBER form (0 to 7).
+     * @param {Number} idx_destination_x The x coordinate destination in NUMBER form (0 to 7).
+     * @param {Number} idx_destination_y The y coordinate destination in NUMBER form (0 to 7).
      * @param {Array} chessboard The array containing all the active game pieces currently on the chessboard.
      * @param {Object} otherConditions Optional conditions for special pieces just in case.
      */
-    isValidMovement(destination_x, destination_y, chessboard = [], otherConditions) {
+    isValidMovement(idx_destination_x, idx_destination_y, chessboard = [], otherConditions) {
 
-        if (isNaN(destination_x) || isNaN(destination_y)) {
+        if (isNaN(idx_destination_x) || isNaN(idx_destination_y)) {
             throw new Error(this.name + ": expecting destination x and y coordinates in Number form.");
         }
 
-        const result = false;
+        let result = false;
 
-        if (this.faction === this.faction_white) {
-            result = this.__isValidMovement(destination_x, destination_y, 1, conditions);
-        } else if (this.faction === this.faction_black) {
-            result = this.__isValidMovement(destination_x, destination_y, -1, conditions);
+        if (this.faction === this.name_white) {
+            result = this.__isValidMovement(idx_destination_x, idx_destination_y, 1, chessboard);
+        } else if (this.faction === this.name_black) {
+            result = this.__isValidMovement(idx_destination_x, idx_destination_y, -1, chessboard);
         }
  
         return result;
