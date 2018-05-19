02d45055c05da45e019eac93f5e91fdf53fdd5b5
jimmyh3 <jimmyhe3452@gmail.com>
2018-05-18 19:20:50 -0700

    Implemented Piece.getFirstPieceScan() to get the first Piece toward a given direction from an origin. Basically this is 2D Raycasting.

--- models/chess_pieces/piece.js
+++ models/chess_pieces/piece.js
@@ -27,7 +27,8 @@ class Piece {
     /**
      * Determines if the given destinations are valid positions the piece can move to.
      * The Pieces will also determine if there are blocking pieces in its path that prevents
-     * it from moving the destination.
+     * it from moving the destination. The Piece will also validate if the destination piece
+     * is of the opposing faction.
      * @param {Number} idx_destination_x The x coordinate destination in NUMBER form (0 to 7).
      * @param {Number} idx_destination_y The y coordinate destination in NUMBER form (0 to 7).
      * @param {Array} chessboard The array containing all the active game pieces currently on the chessboard.
@@ -36,6 +37,29 @@ class Piece {
      */
     isValidMovement(idx_destination_x, idx_destination_y, chessboard = [], otherConditions) {
         throw new Error(this.isValidMovement.name + " is abstract and must be implemented.");
+    }
+
+    /**
+     * Find the first Piece object scanning toward the given direction.
+     * @param {Number} origin_x The starting x-coordinate position (Note that this value is incremented by origin_incX at start).
+     * @param {Number} origin_y The starting y-coordinate position (Note that this value is incremented by origin_incY at start).
+     * @param {Number} origin_incX The increment to add to the origin x-coordinate.
+     * @param {Number} origin_incY The increment to add to the origin y-coordinate.
+     * @param {Array[]} chessboard The chessboard 2D array where Pieces are located in.
+     * @param {Number} chessboardSize The chessboard dimension.
+     * @return {(Piece|undefined)} The first hit object in the scan or undefined if the chessboard size was reached.
+     */
+    static getFirstPieceScan(origin_x, origin_y, origin_incX, origin_incY, chessboard, chessboardSize) {
+        let hitPiece = undefined;
+
+        for (let col = origin_x + origin_incX, row = origin_y + origin_incY; (col < chessboardSize && row < chessboardSize); col += origin_incX, row += origin_incY) {
+            hitPiece = chessboard[col][row];
+            if (hitPiece != undefined) {
+                break;
+            }
+        }
+
+        return hitPiece;
     }
 
     /**
