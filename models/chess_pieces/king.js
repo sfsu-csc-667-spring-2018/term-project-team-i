46b79c93619a6d118a2deba030cf4706f15d9a2c
Razmik Hakobyan <razhakobyan@gmail.com>
2018-05-19 17:01:51 -0700

    removed error

--- models/chess_pieces/king.js
+++ models/chess_pieces/king.js
@@ -231,7 +231,6 @@ class King extends Piece{
                             }
                         }
                     } else {
-                        if ()
                     }
                 }
             }
